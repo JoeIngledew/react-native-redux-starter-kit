8d01134c1e6fd5d53001764d4ba3a6dec33072bc
Joe Ingledew <ingledewjf@gmail.com>
2018-07-12 13:57:54 +0100

    added a whole load of dashboard stuff

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -2,13 +2,36 @@ import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet} from 'react-native'
 
 const Dashboard = (props) => {
-  const { value, increment, doubleAsync } = props;
+  const { value, list, onChangeText, submitAction, itemOnEdit, textInput, buttonText } = props;
+
+  const inputSubmitAction = () => {
+    submitAction();
+  }
+
+  const editLabelAction = (index) => {
+    itemOnEdit(index);
+  }
+
+  const listJSX = list.map((item, i) => {
+    return <TouchableOpacity key={i} onPress={() => editLabelAction(i)}>
+             <Text style={styles.buttonText}>{item.label}</Text>
+           </TouchableOpacity>
+  })
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
         Dashboard visits: <Text style={styles.value}>{value}</Text>
       </Text>
+      <TextInput
+        style={styles.inputText}
+        onChange={(text) => onChangeText(text)}
+        value={textInput}
+        placeholder='Input new label here' />
+      <TouchableOpacity onPress={inputSubmitAction} style={styles.button}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+      {listJSX}
     </View>
   )
 }
@@ -29,6 +52,29 @@ const styles = StyleSheet.create({
     width: 40,
     fontWeight: 'bold',
     color: 'limegreen',
+    textAlign: 'center',
+  },
+  inputText: {
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+  },
+  button: {
+    paddingLeft: 4,
+    paddingRight: 4,
+    backgroundColor: 'rgb(233, 233, 233)',
+    borderWidth: 1,
+    borderColor: 'rgb(213, 213, 213)',
+    margin: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  item: {
+    fontSize: 20,
     textAlign: 'center',
   },
 })
--- src/routes/Dashboard/modules/dashboardReducer.js
+++ src/routes/Dashboard/modules/dashboardReducer.js
@@ -1,16 +1,38 @@
 const DASHBOARD_VISITS_INCREMENT = 'DASHBOARD_VISITS_INCREMENT'
+const DASHBOARD_ADD_ITEM = 'DASHBOARD_ADD_ITEM'
+const DASHBOARD_EDIT_ITEM = 'DASHBOARD_EDIT_ITEM'
 
 export const visitsIncrement = (value = 1) => ({
   type: DASHBOARD_VISITS_INCREMENT,
   value
 })
 
-const initialState = 0
+export const dashboardAddItem = (label) => ({
+  type: DASHBOARD_ADD_ITEM,
+  label
+})
+
+export const DASHBOARD_EDIT_ITEM = (label, index) => ({
+  type: DASHBOARD_EDIT_ITEM,
+  label,
+  index
+})
+
+const initialState = {
+  visitsCount: =,
+  list: [
+    { key: 0, label: 'Angular' },
+    { key: 1, label: 'VanillaJS' },
+    { key: 2, label: 'Polymer' },
+    { key: 3, label: 'React' },
+  ]
+}
 
 export default function dashboard(state = initialState, action) {
   switch (action.type) {
     case DASHBOARD_VISITS_INCREMENT:
-      return state + action.value
+      state.visitsCount = state.visitsCount + action.value
+      return Object.assign({}, state)
     default:
       return state
   }
